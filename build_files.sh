27ca5460b0620ec54e9a0d56ce333beb04aa5602
mvip_07 <mirabzalozodov07@gmail.com>
2023-10-25 11:09:26 +0500

    1

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+echo " BUILD START"
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+echo " BUILD END"
